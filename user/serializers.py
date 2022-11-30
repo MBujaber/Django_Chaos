a5ac90ad1914882eb71ab5ee7ad5e3481618c11e
MBujaber <89960384+MBujaber@users.noreply.github.com>
2022-11-30 19:31:27 +0300

    first commit

---
+++ user/serializers.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+
+    def create(self, validated_data):
+        username = validated_data["username"]
+        password = validated_data["password"]
+        new_user = User(username=username)
+        new_user.set_password(password)
+        new_user.save()
+        return new_user
+
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    access = serializers.CharField(allow_blank=True, read_only=True)
+    def validate(self, data):
+        my_username = data.get("username")
+        my_password = data.get("password")
+
+        try:
+            user = User.objects.get(username=my_username)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("This username does not exist")
+
+        if not user.check_password(my_password):
+            raise serializers.ValidationError("Incorrect username/password combination!")
+
+        payload = RefreshToken.for_user(user)
+        token = str(payload.access_token)
+
+        data["access"] = token
+        return data
+
+
+
+
+        
